a27552c12e5e89f0898e268182b47611d3247c43
maxschaffelder <mschaffelder@gmail.com>
2024-01-12 15:28:39 +0100

    Update model_baseline.py

--- model_baseline.py
+++ model_baseline.py
@@ -1,4 +1,5 @@
 import argparse
+import itertools
 from time import time
 import torch
 import torch.nn as nn
@@ -7,23 +8,28 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 from utils import CLASS_SIZE, DEVICE, FEATURE_SIZE, MEGDatasetType, get_dataloader
 
-# Two layers with 128 do not perform significantly better than with 64 (accuracy = 0.96) for crosstrain cross1test
-# Better than 32 (acc = 0.92)
-# performance of 64 improved to 0.989 with l2 regularization parameter = 0.01
-# dropout does not have a noticeable effect, slightly decreases performance
+# Best hyperparameter config
+# lr = 0.001
+# Batch size = 5000
+# 3 epochs
+# 2 hidden layers
+# 32 neurons per layer
+# dropout 0.5
+# l2 lambda = 0.1
 
-# Intra accuracy = 100%
-# Cross 1 hyperparameters: 2 layers, 64 neurons, L2 = 0.01, accuracy = 0.98 or so
-# Cross 2 accuracy = 0.44
-# Cross 3 accuracy = 0.7
+# Accuracy
+# Intra: 100%
+# Cross1: 96.8%
+# Cross2: 65.3%
+# Cross3: 68.7%
 
 # Data
 sequence_length = 1
 shuffle = True
 
 # Training
-num_epochs = 2
-batch_size = 500
+num_epochs = 3
+batch_size = 5000
 learning_rate = 0.001
 loss_function = nn.CrossEntropyLoss()
 optimizer_function = torch.optim.Adam
@@ -31,13 +37,13 @@ optimizer_function = torch.optim.Adam
 
 # Define Neural Network
 class FFNN(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):#, dropout_rate=0.3):
+    def __init__(self, input_size, hidden_size, num_classes, dropout_rate=0.5):
         super(FFNN, self).__init__()
         # Hidden Layers
         self.hidden1 = nn.Linear(input_size, hidden_size)
-        #self.dropout1 = nn.Dropout(dropout_rate)
+        self.dropout1 = nn.Dropout(dropout_rate)
         self.hidden2 = nn.Linear(hidden_size, hidden_size)
-        #self.dropout2 = nn.Dropout(dropout_rate)
+        self.dropout2 = nn.Dropout(dropout_rate)
         # Activation function
         self.relu = nn.ReLU()
         # Output layer
@@ -46,10 +52,10 @@ class FFNN(nn.Module):
     def forward(self, x):
         out = self.hidden1(x)
         out = self.relu(out)
-        #out = self.dropout1(out)
+        out = self.dropout1(out)
         out = self.hidden2(out)
         out = self.relu(out)
-        #out = self.dropout2(out)
+        out = self.dropout2(out)
         out = self.output(out)
         return out
 
@@ -75,8 +81,6 @@ def train_model(
             X = X.float().to(device)
             y = y.long().to(device)
             preds = model(X)
-            #print(X.shape)
-            #print(y.shape, preds.shape)
             preds = preds.squeeze(1)
             loss = loss_fn(preds, y)
             train_losses.append(loss.item())
@@ -150,10 +154,10 @@ def main():
     args = parser.parse_args()
     model = FFNN(
         input_size=FEATURE_SIZE,
-        hidden_size=64,
+        hidden_size=32,
         num_classes=CLASS_SIZE,
     )
-    optimizer = optimizer_function(model.parameters(), lr=learning_rate, weight_decay=0.01)
+    optimizer = optimizer_function(model.parameters(), lr=learning_rate, weight_decay=0.1)
 
     if args.train_set:
         train_dataloader = get_dataloader(
@@ -188,7 +192,58 @@ def main():
 
         test_model(test_dataloader, model, loss_function, DEVICE)
 
-    if not args.test_set and not args.train_set:
+    """if args.hyperparameter_tuning:
+        hyperparameters = {
+            'learning_rates': [0.001, 0.01, 0.1],
+            'batch_size': [256, 500, 2500, 5000],
+            'epochs': [1, 2, 3, 4],
+            'hidden_size': [32, 64, 128]
+        }
+
+        hyperparameter_combinations = list(itertools.product(*[hyperparameters[key] for key in hyperparameters]))
+        print(len(hyperparameter_combinations), hyperparameter_combinations)
+
+        for lr, batch_size, epochs, hidden_size in hyperparameter_combinations:
+            model = FFNN(
+                input_size=FEATURE_SIZE,
+                hidden_size=hidden_size,
+                num_classes=CLASS_SIZE,
+            )
+            optimizer = optimizer_function(model.parameters(), lr=lr)
+
+            train_dataloader = get_dataloader(
+                train_map[args.hyperparameter_tuning[0]],
+                batch_size=batch_size,
+                shuffle=shuffle,
+                load_all_data=True,
+            )
+
+            test_dataloader = get_dataloader(
+                test_map[args.hyperparameter_tuning[1]],
+                batch_size=batch_size,
+                shuffle=shuffle,
+                load_all_data=True,
+            )
+
+            train_model(
+                model,
+                train_dataloader,
+                loss_function,
+                optimizer,
+                num_epochs=epochs,
+                device=DEVICE,
+            )
+
+            loss, accuracy = test_model(test_dataloader, model, loss_function, DEVICE)
+
+            with open('baseline_hyperparameter_tuning_results.txt', 'a') as file:
+                file.write(f"Learning rate: {lr}, Batch size: {batch_size}, Epochs: {epochs}, Hidden Size: {hidden_size} Test loss: {loss}, Accuracy: {accuracy}\n")
+
+            torch.save(model, f"checkpoints/lr{lr}_batch_size{batch_size}_epochs{epochs}_hidden_size{hidden_size}_model_baseline.pth")
+
+        print("Hyperparameter tuning complete")"""
+
+    if not args.test_set and not args.train_set:# and not args.hyperparameter_tuning:
         print("No Options Selected!!")
 
 
